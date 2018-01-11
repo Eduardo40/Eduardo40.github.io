5ad297b814802190d3bbfdc64fb945f281399ced
eduardo <eduardo1@net.hr>
2018-01-11 09:08:15 +0100

    fix scroll, imporve text

--- 890ddc37ef66aa5a6de3f45227076793.js
+++ 890ddc37ef66aa5a6de3f45227076793.js
@@ -33,20 +33,23 @@ require = (function (modules, cache, entry) {
         err.code = 'MODULE_NOT_FOUND';
         throw err;
       }
-
-      function localRequire(x) {
-        return newRequire(localRequire.resolve(x));
-      }
-
-      localRequire.resolve = function (x) {
-        return modules[name][1][x] || x;
-      };
+      
+      localRequire.resolve = resolve;
 
       var module = cache[name] = new newRequire.Module;
+
       modules[name][0].call(module.exports, localRequire, module, module.exports);
     }
 
     return cache[name].exports;
+
+    function localRequire(x){
+      return newRequire(localRequire.resolve(x));
+    }
+
+    function resolve(x){
+      return modules[name][1][x] || x;
+    }
   }
 
   function Module() {
@@ -65,9 +68,9 @@ require = (function (modules, cache, entry) {
 
   // Override the current require with this new one
   return newRequire;
-})({11:[function(require,module,exports) {
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0});const s=exports.help={resize:function(s,e,t,d){s.innerWidth<=768?(e.classList.add("hidden"),d.classList.remove("hidden"),t.classList.remove("hidden")):s.innerWidth>=768&&(t.classList.add("hidden"),d.classList.add("hideDropdown"),d.classList.add("hidden"),e.classList.remove("hidden"))},getUserInfo:async function(s){try{const e=await fetch("https://api.github.com/users/Eduardo40/repos");return s(null,await e.json())}catch(e){return s(e,null)}}};
-},{}],10:[function(require,module,exports) {
+})({23:[function(require,module,exports) {
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0});const e=exports.help={resize:function(e,s,t,d){e.innerWidth<=768?(s.classList.add("hidden"),d.classList.remove("hidden"),t.classList.remove("hidden")):e.innerWidth>=768&&(t.classList.add("hidden"),d.classList.add("hideDropdown"),d.classList.add("hidden"),s.classList.remove("hidden"))},getGitHubRepo:async function(e){try{const s=await fetch("https://api.github.com/users/Eduardo40/repos");return e(null,await s.json())}catch(s){return e(s,null)}}};
+},{}],20:[function(require,module,exports) {
 var global = (1,eval)("this");
-function e(e=0,n){n.preventDefault(),window.scroll({top:e,behavior:"smooth"})}var n=(0,eval)("this"),t=require("./helper.js");const o=document.querySelector("body .dropdown-nav"),i=document.querySelector(".main-nav"),r=document.querySelector(".control"),d=document.querySelector(".dropdown-nav ul"),l=document.querySelectorAll(".nav-link");n.addEventListener("DOMContentLoaded",t.help.resize.bind(void 0,window,i,r,o)),n.addEventListener("resize",t.help.resize.bind(void 0,window,i,r,o)),r.addEventListener("click",function(){Array.from(o.classList);l.forEach(e=>{e.classList.toggle("hidden")}),d.classList.toggle("showDropDown"),d.classList.toggle("hideDropDown"),o.classList.toggle("showDropDown"),o.classList.toggle("hideDropDown")});const c=document.querySelector("#project-list");t.help.getUserInfo(function(e,n){e||n.forEach(e=>{const n=document.createElement("div");n.setAttribute("class","project"),n.innerHTML=`\n    <h3><a target="_blank" href="${e.html_url}">${e.name}</a></h3>\n    <p>${e.description}</p>\n    <br />\n    <a target="_blank" class='gray' href="${e.html_url}">See Code on <i class="fa fa-github fa-2x"></i></a>\n    ${e.homepage?"<br /><a class=\"gold\" target='_blank' href="+`${e.homepage}`+">See Live</a>":""}\n  `,c.appendChild(n)})});const a=document.querySelectorAll('a[href="#top"]'),s=document.querySelectorAll('a[href="#skills"]'),h=document.querySelectorAll('a[href="#projects"]'),u=document.querySelectorAll('a[href="#contact"]');a[0].addEventListener("click",e.bind(void 0,0)),a[1].addEventListener("click",e.bind(void 0,0)),s[0].addEventListener("click",e.bind(void 0,730)),s[1].addEventListener("click",e.bind(void 0,650)),h[0].addEventListener("click",e.bind(void 0,1300)),h[1].addEventListener("click",e.bind(void 0,2150)),u[0].addEventListener("click",e.bind(void 0,500*window.innerHeight)),u[1].addEventListener("click",e.bind(void 0,500*window.innerHeight));
-},{"./helper.js":11}]},{},[10])
+var e=(0,eval)("this"),t=require("./helper");const n=document.querySelector("body .dropdown-nav"),o=document.querySelector(".main-nav"),r=document.querySelector(".control"),i=document.querySelector(".dropdown-nav ul"),c=document.querySelectorAll(".nav-link");e.addEventListener("DOMContentLoaded",t.help.resize.bind(void 0,window,o,r,n)),e.addEventListener("resize",t.help.resize.bind(void 0,window,o,r,n)),r.addEventListener("click",function(){Array.from(n.classList);c.forEach(e=>{e.classList.toggle("hidden")}),i.classList.toggle("showDropDown"),i.classList.toggle("hideDropDown"),n.classList.toggle("showDropDown"),n.classList.toggle("hideDropDown")});const l=document.querySelector("#project-list");t.help.getGitHubRepo(function(e,t){e||t.forEach(e=>{const t=document.createElement("div");t.setAttribute("class","project"),t.innerHTML=`\n    <h3><a target="_blank" href="${e.html_url}">${e.name}</a></h3>\n    <p>${e.description}</p>\n    <br />\n    <a target="_blank" class='gray' href="${e.html_url}">See Code on <i class="fa fa-github fa-2x"></i></a>\n    ${e.homepage?"<br /><a class=\"gold\" target='_blank' href="+`${e.homepage}`+">See Live</a>":""}\n  `,l.appendChild(t)})});const d=document.querySelectorAll('a[href="#top"]'),a=document.querySelectorAll('a[href="#skills"]'),s=document.querySelectorAll('a[href="#projects"]'),u=document.querySelectorAll('a[href="#contact"]');d[0].addEventListener("click",v.bind(void 0,"#top")),d[1].addEventListener("click",v.bind(void 0,"#top")),a[0].addEventListener("click",v.bind(void 0,"#skills")),a[1].addEventListener("click",v.bind(void 0,"#skills")),s[0].addEventListener("click",v.bind(void 0,"#projects")),s[1].addEventListener("click",v.bind(void 0,"#projects")),u[0].addEventListener("click",v.bind(void 0,"#contact")),u[1].addEventListener("click",v.bind(void 0,"#contact"));function v(e,t){t.preventDefault(),document.querySelector(e).scrollIntoView({behavior:"smooth",block:"start"})}
+},{"./helper":23}]},{},[20])
